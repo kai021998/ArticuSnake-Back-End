15e1b0840131e6ad2218cdd97b6c559905fbd9a8
seanraad123 <seanconrad123@gmail.com>
2018-08-21 13:10:16 -0400

    added a bunch of stuff

---
+++ ArticuSnake/app/controllers/api/v1/sentences_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SentencesController < ApplicationController
+
+  def index
+    sentences = Sentence.all
+    render json: sentences
+  end
+
+  def show
+    sentence = Sentence.find(params[:id])
+    render json: sentence
+  end
+
+end
